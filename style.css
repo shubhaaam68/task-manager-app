55ec393a7b0ac45a7f9647cb261cec70592552e1
Shubham Kumar Singh <shubhamkumarsingh200505@gmail.com>
2026-05-31 12:40:00 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,33 @@
+body{
+    font-family: Arial;
+    background:#f4f4f4;
+    text-align:center;
+}
+
+.box{
+    background:white;
+    width:300px;
+    margin:20px auto;
+    padding:20px;
+    border-radius:10px;
+    box-shadow:0 0 10px gray;
+}
+
+input{
+    width:90%;
+    padding:10px;
+    margin:10px;
+}
+
+button{
+    padding:10px 20px;
+    background:blue;
+    color:white;
+    border:none;
+    cursor:pointer;
+}
+
+li{
+    list-style:none;
+    margin:10px;
+}
